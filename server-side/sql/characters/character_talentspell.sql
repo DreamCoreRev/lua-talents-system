2351b81b5a770eb680d7927fafcf33d383d295e8
Aurøra <azgath.gaming@gmail.com>
2026-05-10 19:29:47 +0200

    Client/DB: Add Exe & Updated Table

--- server-side/sql/characters/character_talentspell.sql
+++ server-side/sql/characters/character_talentspell.sql
@@ -3,9 +3,10 @@
 -- ----------------------------
 DROP TABLE IF EXISTS `character_talentspell`;
 CREATE TABLE `character_talentspell`  (
-  `guid` int(11) UNSIGNED NOT NULL,
-  `spell` int(11) UNSIGNED NOT NULL,
-  `active` tinyint(1) UNSIGNED NOT NULL DEFAULT 0,
+  `guid` int(0) UNSIGNED NOT NULL,
+  `account_id` int(0) UNSIGNED NOT NULL DEFAULT 0,
+  `spell` int(0) UNSIGNED NOT NULL,
+  `active` tinyint(0) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`guid`, `spell`, `active`) USING BTREE,
   UNIQUE INDEX `unique_talent`(`guid`, `spell`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
